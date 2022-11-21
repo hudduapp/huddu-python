8b460c6d41ffb9523e444d85eee75d728debca2d
Joshua3212 <49119664+Joshua3212@users.noreply.github.com>
2022-11-21 18:05:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 from setuptools import setup
 
+readme_file = open("Readme.md", "r")
+readme_content = readme_file.read()
+
 setup(
     name="huddu",
+
     version="1.0",
+
     packages=["huddu"],
-    url="",
+
+    url="https://github.com/hudduapp/huddu-python",
+
     requires=["json", "requests"],
+
     license="MIT",
+
     author="Joshua3212",
+
     author_email="",
+
     description="",
+
+    long_description_content_type="text/markdown",
+
+    long_description=readme_content,
 )
